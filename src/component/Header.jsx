609d69ec11d525b239cd26dbfcc8dbae0a8a21a3
Shubik Bhatt <shubikbhatt2h7h@gmail.com>
2024-12-11 20:53:38 +1100

    Main Section added

---
+++ src/component/Header.jsx
@@ -0,0 +1,16 @@
+export default function Header() {
+  return (
+    <>
+      <div className='wrapper'>
+        <div className='container pt-5'>
+          {/* <!-- header row --> */}
+          <div className='row p-5'>
+            <div className='col text-center text-white font-weight-bolder '>
+              <h1>Not To Do List </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
